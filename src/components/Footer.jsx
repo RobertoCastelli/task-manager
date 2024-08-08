17791f32114570719eced6c7ef50735da70829d3
RobertoCastelli <roberto.castelli.teal@gmail.com>
2024-08-09 00:23:33 +0200

    Boilerplate

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaReact, FaGithub } from "react-icons/fa";
+import { SiNetlify, SiFirebase } from "react-icons/si";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Task Manager - AGIS @2024 v.1.08</div>
+      <div>
+        <FaReact />
+        <FaGithub />
+        <SiNetlify />
+        <SiFirebase />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>logo</div>
+      <ul>
+        <li>login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
